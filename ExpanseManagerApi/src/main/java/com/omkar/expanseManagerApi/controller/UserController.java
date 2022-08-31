003e274ea742389b6aa6795ef02f440fe9d7527c
Omkar Kumar <Omkarkumar329@gmail.com>
2022-08-31 22:08:54 +0530

    USer validation

--- ExpanseManagerApi/src/main/java/com/omkar/expanseManagerApi/controller/UserController.java
+++ ExpanseManagerApi/src/main/java/com/omkar/expanseManagerApi/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.omkar.expanseManagerApi.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +22,7 @@ public class UserController {
 	
 	
 	@PostMapping("/register")
-     public ResponseEntity<User> Save(@RequestBody UserModel user)
+     public ResponseEntity<User> Save(@Valid @RequestBody UserModel user)
      {
     	
     	 return new ResponseEntity<User>(userService.createUser(user),HttpStatus.CREATED);
--- ExpanseManagerApi/src/main/java/com/omkar/expanseManagerApi/entity/UserModel.java
+++ ExpanseManagerApi/src/main/java/com/omkar/expanseManagerApi/entity/UserModel.java
@@ -1,13 +1,24 @@
 package com.omkar.expanseManagerApi.entity;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class UserModel {
 	
+	@NotBlank(message = "Please enter name")
 	private String name;
+	@NotNull(message = "Please enter email")
+	@Email(message = "Please enter valid email")
 	private String email;
+	
+	@NotNull(message = "Please enter password")
+	@Size(min = 5, message = "Password should be atleast 5 characters")
 	private String password;
-	private Long age;
+	private Long age= 0L;
 
 }
